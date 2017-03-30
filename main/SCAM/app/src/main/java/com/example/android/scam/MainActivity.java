9fa90b89372088e933479cf997b85288c1721ccd
Juan Castillo <juan.a.castillo@sjsu.edu>
2017-03-30 16:29:25 -0700

    Firebase Integration

--- main/SCAM/app/src/main/java/com/example/android/scam/MainActivity.java
+++ main/SCAM/app/src/main/java/com/example/android/scam/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.android.scam;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Listen for auth state
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //String
+    private final String TAG = "auth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get Firebase auth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        // Check for signed in
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+
+        }
+
     }
+
 }
